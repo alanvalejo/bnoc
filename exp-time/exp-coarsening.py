106fb97d92bc3dc59ea307f002a82f4dd5a75c9c
Alan Valejo <alanvalejo@gmail.com>
2018-05-04 11:35:06 -0300

    update

--- exp-time/exp-coarsening.py
+++ exp-time/exp-coarsening.py
@@ -5,7 +5,7 @@ rset = [0.1, 0.2, 0.3, 0.4, 0.5]
 mset = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 d = 'result/'
 
-for index, v in enumerate(range(1000, 4000, 1000)):
+for index, v in enumerate(range(1000, 101000, 1000)):
 	v1 = (v / 2)
 	v2 = (v / 2)
 	f = 'dataset/' + str(index + 1) + '.ncol'
@@ -13,15 +13,15 @@ for index, v in enumerate(range(1000, 4000, 1000)):
 		for m in mset:
 			for r in rset:
 				o = c + '_' + str(v) + '_' + str(r).replace('.', '')
-				print('python ../../opm/coarsening.py -f ' + f + ' -v ' + str(v1) + ' ' + str(v2) + ' -c ' + str(c) + ' -m ' + str(m) + ' -r ' + str(r) + ' -d ' + str(d) + ' -o ' + str(o) + ' --save_timing_json')
-				os.system('python ../../opm/coarsening.py -f ' + f + ' -v ' + str(v1) + ' ' + str(v2) + ' -c ' + str(c) + ' -m ' + str(m) + ' -r ' + str(r) + ' -d ' + str(d) + ' -o ' + str(o) + ' --save_timing_json')
+				print('python ../../opm-src/coarsening.py -f ' + f + ' -v ' + str(v1) + ' ' + str(v2) + ' -c ' + str(c) + ' -m ' + str(m) + ' -r ' + str(r) + ' -d ' + str(d) + ' -o ' + str(o) + ' --save_timing_json')
+				os.system('python ../../opm-src/coarsening.py -f ' + f + ' -v ' + str(v1) + ' ' + str(v2) + ' -c ' + str(c) + ' -m ' + str(m) + ' -r ' + str(r) + ' -d ' + str(d) + ' -o ' + str(o) + ' --save_timing_json')
 
 cset = ['greedy_twohops', 'greedy_seed_twohops']
 rset = [0.1, 0.2, 0.3, 0.4, 0.5]
 mset = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 d = 'result/'
 
-for index, v in enumerate(range(1000, 4000, 1000)):
+for index, v in enumerate(range(1000, 101000, 1000)):
 	v1 = (v / 2)
 	v2 = (v / 2)
 	f = 'dataset/' + str(index + 1) + '.ncol'
@@ -37,7 +37,7 @@ rset = [0.1, 0.2, 0.3, 0.4, 0.5]
 mset = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 d = 'result/'
 
-for index, v in enumerate(range(1000, 4000, 1000)):
+for index, v in enumerate(range(1000, 101000, 1000)):
 	v1 = (v / 2)
 	v2 = (v / 2)
 	f = 'dataset/' + str(index + 1) + '.ncol'
@@ -53,7 +53,7 @@ rset = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 mset = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 d = 'result/'
 
-for index, v in enumerate(range(1000, 4000, 1000)):
+for index, v in enumerate(range(1000, 101000, 1000)):
 	v1 = (v / 2)
 	v2 = (v / 2)
 	f = 'dataset/' + str(index + 1) + '.ncol'
