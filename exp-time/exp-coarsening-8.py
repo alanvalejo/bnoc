0c559e982d51fd3b7c21f6d4d114bf0264ca145b
alanvalejo <alanvalejo@gmail.com>
2018-05-04 11:39:09 -0300

    update

---
+++ exp-time/exp-coarsening-8.py
@@ -0,0 +1,17 @@
+import os
+
+cset = ['lpa_modularity']
+rset = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+mset = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+d = 'result/'
+
+for index, v in enumerate(range(1000, 101000, 1000)):
+	v1 = (v / 2)
+	v2 = (v / 2)
+	f = 'dataset/' + str(index + 1) + '.ncol'
+	for c in cset:
+		for m in mset:
+			for r in rset:
+				o = c + '_' + str(v) + '_' + str(r).replace('.', '')
+				print('python ../../mob_exp/coarsening.py -f ' + f + ' -v ' + str(v1) + ' ' + str(v2) + ' -c ' + str(c) + ' -m ' + str(m) + ' -r ' + str(r) + ' -d ' + str(d) + ' -o ' + str(o) + ' --save_timing_json')
+				os.system('python ../../mob_exp/coarsening.py -f ' + f + ' -v ' + str(v1) + ' ' + str(v2) + ' -c ' + str(c) + ' -m ' + str(m) + ' -r ' + str(r) + ' -d ' + str(d) + ' -o ' + str(o) + ' --save_timing_json')
